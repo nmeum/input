f8a0ebf7af12a1b21cfeb44b6180a352ac251eeb
Sören Tempel <soeren+git@soeren-tempel.net>
2019-10-28 23:47:12 +0100

    input: block all signals in atexit(3) handler
    This fixes a race condition I occasionally witnessed on OpenBSD where a
    signal would be delivered to the input process. This signal would cause
    el_gets(3) to fail with EINTR, thereby causing input to call err(3)
    which would invoke cleanup() at exit. During the invocation of cleanup()
    a signal would be delivered to the process causing it to jump to the
    signal handler before completing the cleanup operation. The result would
    often be a corrupted history file.

--- input.c
+++ input.c
@@ -67,6 +67,23 @@ cleanup(void)
 }
 
 static void
+onexit(void)
+{
+	sigset_t blockset;
+
+	/* cleanup is called atexit(3) and from a signal handler, block
+	 * all signals here before invoking cleanup to ensure we are not
+	 * interrupted by the signal handler during cleanup. */
+
+	if (sigfillset(&blockset) == -1)
+		err(EXIT_FAILURE, "sigemptyset failed");
+	if (sigprocmask(SIG_BLOCK, &blockset, NULL))
+		err(EXIT_FAILURE, "sigprocmask failed");
+
+	cleanup();
+}
+
+static void
 sighandler(int num)
 {
 	(void)num;
@@ -275,7 +292,7 @@ main(int argc, char **argv)
 	el_source(el, NULL); /* source user defaults */
 
 	sethandler();
-	if (atexit(cleanup))
+	if (atexit(onexit))
 		err(EXIT_FAILURE, "atexit failed");
 
 	if (histfp)
